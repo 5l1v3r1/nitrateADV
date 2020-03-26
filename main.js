2a2845cbd3f6a0c35f1eaea0e01a6edd0aed5709
[Cloud Chaser] Freak <33170633+Freak-chan@users.noreply.github.com>
2020-03-25 23:27:04 -0700

    Added invite server support. Check CONFIG.JSON

--- main.js
+++ main.js
@@ -6,71 +6,83 @@ const { default: { post } } = require('axios'),
     notifier = require("node-notifier");
 
 let tokens = fs.readFileSync("./tokens.txt", "utf8").replace(/\r/, "").split('\n');
-
 let count = 0;
-
-let repeated = [];
+let repeatedCodes = [];
+let repeatedInvites = [];
 
 for (token of tokens) {
 
     const bot = new Client();
-
     bot.on("ready", () => {
         console.log(`Logged in as: ${chalk.yellow(bot.user.tag)}\nEmail: ${chalk.bold(bot.user.email)}\nID: ${chalk.bold(bot.user.id)}\n\n`);
         process.title = `${bot.user.tag} | ${bot.guilds.size} guilds | ${bot.user.friends.size} friends`;
-
     });
-
     bot.on("message", async (message) => {
-
         try {
 
-            if (message.channel.type === "dm" || message.channel.type === "group") return;
+            if ( config.joinServers ) {
+                let invites = message.content.match(/(discord.gg|discordapp.com\/invites)\/\w+/gi);
+                if ( invites === [] ) {
+                    return;
+                };
 
-            if (!(message.content.includes("discord.gift/") || message.content.includes("discordapp.com/gifts/"))) return;
+                for ( let urls of invites ) {
+                    let code; 
+                    if ( urls.includes('invites') ) {
+                        code = urls.split('invites')[1]
+                    } else {
+                        code = urls.split('/')[1]
+                    }
 
-            let codes = message.content.match(/(discord.gift|discordapp.com\/gifts)\/[a-zA-Z0-9]{16,24}/g);
+                    if ( repeatedInvites.includes(code) ) return;
+                    repeatedInvites.push(code)
 
-            if (codes === []) {
+                    await post(`https://discordapp.com/api/v6/invites/${code}`, {}, {
+                        headers: {
+                            "Authorization": message.client.token,
+                            "user-agent": "Mozilla/5.0"
+                        }
+                    }).catch(O_o=>{});
+                }
+            }
+            if ( message.channel.type === "dm" || message.channel.type === "group" ) return;
+
+            if ( !(message.content.includes("discord.gift/") || message.content.includes("discordapp.com/gifts/")) ) return;
+
+            let codes = message.content.match( /(discord.gift|discordapp.com\/gifts)\/[a-zA-Z0-9]{16,24}/g );
+
+            if ( codes === [] ) {
                 return;
             }
 
-            for (let gift of codes) {
-
+            for ( let gift of codes ) {
                 let giftCode = gift.split("/")[1];
-
-                if (repeated.includes(giftCode)) {
+                if ( repeatedCodes.includes(giftCode) ) {
                     console.log(chalk.redBright("INVALID") + ` ${code} - Already attempted`)
                     return;
                 };
 
-                repeated.push(giftCode);
-
+                repeatedCodes.push( giftCode );
                 count += 1;
-   
                 process.title = `${message.client.user.tag} | ${message.client.guilds.size} guilds | ${message.client.user.friends.size} friends | ${count.toString()} gift(s)`
             
-                await redeem(giftCode, message);
+                await redeem( giftCode, message );
 
             }
-
- 
-        } catch (e) {
+        } catch ( e ) {
             ''
         }
     });
 
     try {
-        bot.login(token)
+        bot.login( token )
     } catch (err) {
         continue;
     }
-
 }
 
 let data = {"channel_id": null, "payment_source_id": null};
-
-async function redeem (code, message) {
+async function redeem ( code, message ) {
         let _data = await post('https://discordapp.com/api/v6/entitlements/gift-codes/' + code + '/redeem', data, {
             headers: {
                 "User-Agent": "Mozilla/5.0 (Linux; Android 8.0.0;) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.136 Mobile Safari/537.36",
@@ -78,17 +90,16 @@ async function redeem (code, message) {
                 'Authorization': config.token
             },
             time: true
-        }).catch(_error => {
-            if ( _error.response.status == 404 || _error.response.data['message'] == "Unkown Gift Code") {
+        }).catch( _error => {
+            if ( _error.response.status == 404 || _error.response.data['message'] == "Unkown Gift Code" ) {
                 return console.log(chalk.redBright("INVALID") + ` ${code} - Invalid Code`);
             }
             return;
         });
-        var result = JSON.parse(_data.data);
-        var responseTime = new Date() - start;
-        
-        console.log(chalk.green("VALID") + ` ${result.message} (${responseTime / 1000}s)`);
 
+        var result = JSON.parse( _data.data );
+        var responseTime = new Date() - start;
+        console.log( chalk.green("VALID") + ` ${result.message} (${responseTime / 1000}s)` );
         notifier.notify({
             title: 'Nitrate',
             icon: 'nitro-png-2.png',
