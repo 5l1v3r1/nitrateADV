7a08bafc52b8be3d25a7a70b8fa6b87c0bc68eaa
[Cloud Chaser] Freak <33170633+Freak-chan@users.noreply.github.com>
2020-03-27 00:42:44 -0700

    Updated auto token filter.

--- main.js
+++ main.js
@@ -5,74 +5,69 @@ const { default: { post } } = require('axios'),
     config = JSON.parse(fs.readFileSync("./config.json", "utf8")),
     notifier = require("node-notifier");
 
-let tokens = fs.readFileSync("./tokens.txt", "utf8").replace(/\r/, "").split('\n');
+let tokens = fs.readFileSync("./tokens.txt", "utf8").replace(/\r/g, "").split('\n');
 let count = 0;
 let repeatedCodes = [];
+let title = ' Fweak | Nitro Auto Claimer (og: Giggl3z/Nitrate)';
 
-for (token of tokens) {
-    const bot = new Client();
+function start() {
+    for (token of tokens) {
+        const bot = new Client();
 
-    bot.on("ready", () => {
-        console.log(`Logged in as: ${chalk.yellow(bot.user.tag)}\nEmail: ${chalk.bold(bot.user.email)}\nID: ${chalk.bold(bot.user.id)}\n\n`);
-        process.title = `${bot.user.tag} | ${bot.guilds.size} guilds | ${bot.user.friends.size} friends`;
-    });
+        bot.on("ready", () => {
+            console.log(`Logged in as: ${chalk.yellow(bot.user.tag)}\nEmail: ${chalk.bold(bot.user.email)}\nID: ${chalk.bold(bot.user.id)}\n\n`);
+            process.title = title;
+        });
 
-    bot.on("message", async (message) => {
-        try{ 
+        bot.on("message", async (message) => {
 
-            if ( config.joinServers === true ) {
+            blackListedInvites = fs.readFileSync("blacklistedInvites.txt", 'utf8').split('\n');
 
-                blackListedInvites = fs.readFileSync("blacklistedInvites.txt", 'utf8').split('\n');
+            if (config.joinServers == true) {
 
                 let invites = message.content.match(/(discord.gg|discordapp.com\/invites)\/\w+/gi);
 
-                if ( invites === null || !invites[0] || typeof invites[0] == "null" ) return;
+                if (invites && invites !== null && typeof invites[0] !== "null") {
 
-                for ( let url of invites ) {
+                    invites.forEach((invite, i) => {
 
-                    let code; 
-                    if ( url.includes('invites') ) {
-                        code = url.split('invites')[1]
-                    } else {
-                        code = url.split('/')[1]
-                    }
+                        setTimeout(async () => {
 
-                    if (typeof blackListedInvites.filter(s => s.includes(code))[0] !== "undefined") return;
+                            let code;
 
-                    fs.appendFileSync('blacklistedInvites.txt', `\n${url}`, 'utf8');
+                            if (invite.includes('invites')) {
+                                code = invite.split('invites')[1]
+                            } else {
+                                code = invite.split('/')[1]
+                            }
 
-                    setTimeout( async () => {
+                            if (typeof blackListedInvites.filter(s => s.includes(code))[0] !== "undefined") return;
 
-                    _data = await post(`https://discordapp.com/api/v6/invites/${code}`, {}, {
-                        headers: {
-                            "Authorization": message.client.token,
-                            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
-                        }
-                    }).catch(() => {});
+                            fs.appendFileSync('blacklistedInvites.txt', `${invite}\n`, 'utf8');
+                            _data = await post(`https://discordapp.com/api/v6/invites/${code}`, {}, {
+                                headers: {
+                                    "Authorization": message.client.token,
+                                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+                                }
+                            }).catch(() => { });
 
-                    if ( typeof _data === "undefined" ){
-                    console.log(chalk.red("COULDN'T JOIN INVITE") + " Seems the account is disabled. " + chalk.green(`\nTOKEN: `) + `${message.client.token}\n`)
-                    return; 
-                    };
-                    
-                    if ( _data.data['message'] === "Unknown Invite") return;
+                            if (typeof _data === "undefined" || _data.data['message'] === "Unkown Invite") {
+                                console.log(chalk.red("COULDN'T JOIN INVITE") + ` URL: ${invite}`)
+                                return;
+                            };
 
-                    console.log(` Joined a new server: ` + chalk.green(`${_data.data['guild']['name']}` ))
-
-                }, 5000);
-
+                            console.log(`Joined a new server: ` + chalk.green(`${_data.data['guild']['name']}`))
+                        }, i * 5000);
+                    })
                 }
-                return;
-
-            };  
-
+            }
 
             let codes = message.content.match(/(discord.gift|discordapp.com\/gifts)\/\w{16,24}/);
 
-            if ( codes === null || !codes[0] || typeof codes[0] == "null" ) return;
+            if (codes === null || !codes[0] || typeof codes[0] == "null") return;
 
             let giftCode;
-            
+
             if (codes[0].includes("discordapp.com/gifts/")) {
                 giftCode = codes[0].split('/')[2]
             } else {
@@ -87,47 +82,41 @@ for (token of tokens) {
 
             repeatedCodes.push(giftCode);
             count += 1;
-            process.title = `${message.client.user.tag} | ${message.client.guilds.size} guilds | ${message.client.user.friends.size} friends | ${count.toString()} gift(s)`
-        
+            process.title = title + ` | ${count.toString()} gift(s)`
+
             await redeem(giftCode, message);
 
-        } catch (err) {
-            console.error(err)
-        }
+        });
 
-    });
-
-    try {
         bot.login(token)
-    } catch (err) {
-        '';
-        continue;
+            .catch(err => {
+                if (err && error.message === "Incorrect login details were provided.") {
+                tokens = tokens.filter(s => s !== token);
+                return start()
+                }
+            }).catch(O_o=>{})
+
+
     }
 }
+start()
+let data = { "channel_id": null, "payment_source_id": null };
 
-let data = {"channel_id": null, "payment_source_id": null};
-async function redeem ( code, message ) {
-        let _data = await post('https://discordapp.com/api/v6/entitlements/gift-codes/' + code + '/redeem', data, {
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Linux; Android 8.0.0;) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.136 Mobile Safari/537.36",
-                "Content-Type": "application/json",
-                'Authorization': config.token
-            },
-            time: true
-        }).catch(() => {});
+async function redeem(code) {
+    let _data = await post('https://discordapp.com/api/v6/entitlements/gift-codes/' + code + '/redeem', data, {
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 8.0.0;) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.136 Mobile Safari/537.36",
+            "Content-Type": "application/json",
+            'Authorization': config.token
+        },
+        time: true
+    }).catch(() => { });
 
-        if ( typeof _data === "undefined") {
-            return console.log(chalk.redBright("INVALID") + ` ${code} - Invalid Code`);
-        }
-
-        var result = JSON.parse( _data.data );
-        var responseTime = new Date() - start;
-        console.log( chalk.green("VALID") + ` ${result.message} (${responseTime / 1000}s)` );
-        notifier.notify({
-            title: 'Nitrate',
-            icon: 'nitro-png-2.png',
-            appID: `${message.guild.name} | #${message.channel.name} | ${message.author.tag}`,
-            message: result.message,
-            timeout: 0.1
-        });
+    if (typeof _data === "undefined") {
+        return console.log(chalk.redBright("INVALID") + ` ${code} - Invalid Code`);
     }
+
+    var result = JSON.parse(_data.data);
+    var responseTime = new Date() - start;
+    console.log(chalk.green("VALID") + ` ${result.message} (${responseTime / 1000}s)`);
+}
